1bc9b263ec8ba970524f60b81fdb30821b5b7946
Frank <frankchi24@gmail.com>
2016-02-18 19:13:26 +0800

    Add several new series, add new function to search in multiple series user chooses

--- search_lines.py
+++ search_lines.py
@@ -1,8 +1,15 @@
 # ShowMeEnglish Scripts and content management system
-# Features
-# 1. store subtitles as srt files, search through them when
-# 2. search by each tv series, seasons, episodes, sentences
+# Features now
+# 1. search through stored subtitles(srt files), X
+# 2. able to search by each tv series, seasons
 # 3. search and present words in sentences and episodes
+# 4. print the results as txt files
+
+# Features to add
+# able to search by each multiple series or seasons or episodes
+# able to search phrases and words separately
+# able to determine the how many lines are presented
+# able to choose different stems, or search a lists of words 
 import glob
 import linecache
 import fnmatch
@@ -19,24 +26,58 @@ while counter == 0:
 	text5 = text + "!"
 
 
-	series = ["How I Met Your Mother", "The Big Bang Theory","Game of Thrones","House of Cards","Breaking Bad"]
-	pick_series = int(raw_input('Search in a TV Show? Pick a number.\n0.ALL\n1.How I Met Your Mother\n2.Big Bang Theory\n3.Game of Thrones\n4.House of Cards\n5.Breaking Bad\n> '))
-	if pick_series > 0:
-		series_name = series[int(pick_series)-1]
-	elif pick_series == 0:
-		series_name = "*"
-
-	season_number = input('Which Season? Type 0 if search all seasons\n> ')
-	if season_number > 0:
-		season = "Season %d" % season_number
-	elif season_number == 0:
-		season = "*"
 	#for windows
-	# path = 'C:\Users\Frank/Desktop/ScriptSearch/%s/%s/*.srt' %(series_name,season)
-	#for linux
-	path = '/home/frank/Dropbox/ScriptSearch/%s/%s/*.srt' % (series_name, season)
-	files = glob.glob(path)
+	# os_path = 'C:\Users\Frank/Desktop/ScriptSearch'
+	os_path = '/home/frank/Dropbox/ScriptSearch/'
+
+
+	series = ["How I Met Your Mother", 
+			  "The Big Bang Theory",
+			  "Game of Thrones",
+			  "House of Cards",
+			  "Breaking Bad",
+			  "Mad Men",
+			  "Suits",
+			  "The Newsroom",
+			  "Sex and the City"
+			  ]
+	pick_series = int(raw_input('Search in a TV Show? Pick a number or type 99 to search in multiple series.\
+		\n0.ALL\n1.How I Met Your Mother\n2.The Big Bang Theory\
+		\n3.Game of Thrones\n4.House of Cards\n5.Breaking Bad\
+		\n6.Mad Men\n7.Suits\n8.The Newsroom\n9.Sex and the City\n> '))
+	if pick_series > 0 and pick_series!= 99:
+		series_name = series[int(pick_series)-1]
+		season_number = input('Which Season? Type 0 if search all seasons\n> ')	
+		if season_number > 0:
+			season = "Season %d" % season_number
+		elif season_number == 0:
+			season = "*" 
+		path = os_path + '%s/%s/*.srt' % (series_name, season)
+		files = glob.glob(path)
+	elif pick_series == 0:
+		series_name = "*" 
+		season ="*"
+		path = os_path + '%s/%s/*.srt' % (series_name, season)
+		files = glob.glob(path)
+	elif pick_series == 99:
+		numbers_user_pick = []
+		print "0.ALL\n1.How I Met Your Mother\n2.The Big Bang Theory\
+			\n3.Game of Thrones\n4.House of Cards\n5.Breaking Bad\
+			\n6.Mad Men\n7.Suits\n8.The Newsroom\n9.Sex and the City"
+		user_input = int(raw_input('Type the number of the series.\n>')) 
+		while user_input!=0:			
+			numbers_user_pick.append(user_input)
+			print "---------------------------------------------"
+			print "Here're the series you chose:\n"
+			for number in numbers_user_pick:
+				print series[number-1]
+			user_input = int(raw_input('\nPick again?Type 0 to start search\n>')) 
+		files = []
+		for number in numbers_user_pick:			
+			files = files + glob.glob(os_path +'%s/%s/*.srt' % (series[number],"*"))
+
 	result_list = []
+
 	#print phrases, print our the lines the prases are in
 	def check_phrases():
 		filename = os.path.basename(fp.name)
@@ -62,7 +103,6 @@ while counter == 0:
 	#run check phrases function with each line in each subtitles file 
 	for file in files:
 		fp = open (file, 'r')
- 
 		for i, line in enumerate(fp):
 			# if text = re.findall(r'%s'%text,line.lower())
 			if text1 in line.lower():
