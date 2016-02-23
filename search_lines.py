52a3b28dd01d941db4788a37067eb88dc0a6bfd4
Frank <frankchi24@gmail.com>
2016-02-23 19:21:23 +0800

    Add search range, can now decide how many lines are presented with the result, deleted duplicate files in Mad Mne

--- search_lines.py
+++ search_lines.py
@@ -18,19 +18,10 @@ import re
 #search in one or serveral specific tv series
 counter = 0 
 while counter == 0:
-	text = raw_input('Search a term or a word\n> ').lower() 
-	text1 = text + " "
-	text2 = text + ","
-	text3 = text + "."
-	text4 = text + "?"
-	text5 = text + "!"
-
-
+	text = raw_input('Search a term or a word\n> ').lower()
 	#for windows
 	# os_path = 'C:\Users\Frank/Desktop/ScriptSearch'
 	os_path = '/home/frank/Dropbox/ScriptSearch/'
-
-
 	series = ["How I Met Your Mother", 
 			  "The Big Bang Theory",
 			  "Game of Thrones",
@@ -45,6 +36,9 @@ while counter == 0:
 		\n0.ALL\n1.How I Met Your Mother\n2.The Big Bang Theory\
 		\n3.Game of Thrones\n4.House of Cards\n5.Breaking Bad\
 		\n6.Mad Men\n7.Suits\n8.The Newsroom\n9.Sex and the City\n> '))
+
+	#three different scopes, 
+	#1.pick a tv show and a season and search through them
 	if pick_series > 0 and pick_series!= 99:
 		series_name = series[int(pick_series)-1]
 		season_number = input('Which Season? Type 0 if search all seasons\n> ')	
@@ -54,11 +48,13 @@ while counter == 0:
 			season = "*" 
 		path = os_path + '%s/%s/*.srt' % (series_name, season)
 		files = glob.glob(path)
+	#2.pick all tv shows in the database and search through them all
 	elif pick_series == 0:
 		series_name = "*" 
 		season ="*"
 		path = os_path + '%s/%s/*.srt' % (series_name, season)
 		files = glob.glob(path)
+	#3. pick multiple tv shows and search through 
 	elif pick_series == 99:
 		numbers_user_pick = []
 		print "0.ALL\n1.How I Met Your Mother\n2.The Big Bang Theory\
@@ -74,55 +70,36 @@ while counter == 0:
 			user_input = int(raw_input('\nPick again?Type 0 to start search\n>')) 
 		files = []
 		for number in numbers_user_pick:			
-			files = files + glob.glob(os_path +'%s/%s/*.srt' % (series[number],"*"))
-
-	result_list = []
-
+			files = files + glob.glob(os_path +'%s/%s/*.srt' % (series[number-1],"*"))
 	#print phrases, print our the lines the prases are in
+	# lines_presented = int(raw_input('How many adjacent lines are presented with the results?'))
+	result_list = []
 	def check_phrases():
 		filename = os.path.basename(fp.name)
-		line_before_last_line = linecache.getline(file, i-1)
-		last_line = linecache.getline(file, i)
-		next_line = linecache.getline(file, i+2)
-		print "<Number %d match>" % counter
-		print "<In file '%s'>" %filename
-		print "<In line %d in the file>" % i
-		print line_before_last_line
-		print last_line
-		print line
-		print next_line
-		print "------------------------------------------------------\n"	
-		result_list.append("<Number %d match>" % counter)
-		result_list.append(filename)
-		result_list.append(line_before_last_line)
-		result_list.append(last_line)
-		result_list.append(line)
-		result_list.append(next_line)
-		result_list.append("----------------------\n")	
+		lines_presented = -2			
+		result_list.append(
+		"<Number %d match>\n<In file '%s'>\n<In line %d in the file>" % (counter,filename,i),
+			)
+		while lines_presented <= 6:
+			result_list.append(
+				linecache.getline(file, i+lines_presented)
+				)
+			lines_presented = lines_presented + 1
+		result_list.append(
+		"------------------------------------------------------\n"
+		)
+		for item in result_list:
+			print item
 		linecache.clearcache()
 	#run check phrases function with each line in each subtitles file 
 	for file in files:
 		fp = open (file, 'r')
 		for i, line in enumerate(fp):
-			# if text = re.findall(r'%s'%text,line.lower())
-			if text1 in line.lower():
-				counter = counter + 1
-				check_phrases()
-			elif text2 in line.lower():
-				counter = counter + 1
-				check_phrases()
-			elif text3 in line.lower():
-				counter = counter + 1
-				check_phrases()
-			elif text4 in line.lower():
-				counter = counter + 1
-				check_phrases()
-			elif text5 in line.lower():
+			if re.findall(r'%s\s|%s.|%s\,|%s\?|%s\!'%(text,text,text,text,text),line.lower()):
 				counter = counter + 1
 				check_phrases()
 		fp.close()	
-	print "Total %d matches" % counter
-
+	print "Total %d matches" % counter		
 	#create a txt file with the results
 	if counter > 0:
 		result_fiename = text + ".txt"
@@ -130,9 +107,12 @@ while counter == 0:
 		if make_files == 1:
 			results = open("results/%s"%result_fiename,"w")
 			for item in result_list:
-			  results.write("%s" % item)
+				results.write(item) 
 			results.write( "Total %d matches" % counter)
 			results.close()
 			counter = 0
 		else:
 			counter = 0
+
+
+
