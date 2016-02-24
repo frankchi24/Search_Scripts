ae6ac049c615b998ba199fa6cead0fe2e16d2117
Frank <frankchi24@gmail.com>
2016-02-24 14:54:50 +0800

    put functions to another file

---
+++ check_words.py
@@ -0,0 +1,89 @@
+import glob
+import linecache
+import fnmatch
+import os
+import re
+
+
+def scope(os_path,series,pick_series):
+		if pick_series > 0 and pick_series!= 99:
+			series_name = series[int(pick_series)-1]
+			season_number = input('Which Season? Type 0 if search all seasons\n> ')	
+			if season_number > 0:
+				season = "Season %d" % season_number
+			elif season_number == 0:
+				season = "*" 
+			path = os_path + "%s/%s/*.srt" % (series_name, season)
+			files = glob.glob(path)
+			return files
+
+		#2.pick all tv shows in the database and search through them all
+
+		elif pick_series == 0:
+			series_name = "*" 
+			season ="*"
+			path = os_path + '%s/%s/*.srt' % (series_name, season)
+			files = glob.glob(path)
+			return files
+			 
+		#3. pick multiple tv shows and search through 
+		elif pick_series == 99:
+			numbers_user_pick = []
+			print "0.ALL\n1.How I Met Your Mother\n2.The Big Bang Theory\
+				\n3.Game of Thrones\n4.House of Cards\n5.Breaking Bad\
+				\n6.Mad Men\n7.Suits\n8.The Newsroom\n9.Sex and the City"
+			user_input = int(raw_input('Type the number of the series.\n>')) 
+			while user_input!=0:			
+				numbers_user_pick.append(user_input)
+				print "---------------------------------------------"
+				print "Here're the series you chose:\n"
+				for number in numbers_user_pick:
+					print series[number-1]
+				user_input = int(raw_input('\nPick again?Type 0 to start search\n>')) 
+			files = []
+			for number in numbers_user_pick:			
+				files = files + glob.glob(os_path +'%s/%s/*.srt' % (series[number-1],"*"))
+			return files
+	
+
+#run check phrases function with each line in each subtitles file 
+def check_lines_in_files(files,text,counter,result_list):
+	for file in files:
+		fp = open (file, 'r')
+		for i, line in enumerate(fp):
+			if re.findall(r'%s\s|%s.|%s\,|%s\?|%s\!'%(text,text,text,text,text),line.lower()):
+				counter = counter + 1
+				filename = os.path.basename(fp.name)
+				lines_presented = -2			
+				result_list.append(
+				"<Number %d match>\n<In file '%s'>\n<In line %d in the file>" % (counter,filename,i),
+				)
+				while lines_presented <= 1:
+					result_list.append(
+						linecache.getline(file, i+lines_presented)
+						)
+					lines_presented = lines_presented + 1
+				result_list.append(
+				"------------------------------------------------------\n"
+				)
+				for item in result_list:
+					print item
+				linecache.clearcache()
+		fp.close()	
+	print "Total %d matches" % counter
+	if counter > 0:
+		result_fiename = text + ".txt"
+		make_files =  int(raw_input("Do you wanna print the results out as txt files?\n1.Yes\n2.No\n> "))
+		if make_files == 1:
+			results = open("results/%s"%result_fiename,"w")
+			for item in result_list:
+				results.write(item) 
+			results.write( "Total %d matches" % counter)
+			results.close()
+			counter = 0
+		else:
+			counter = 0
+
+
+
+
--- search_lines.py
+++ search_lines.py
@@ -1,26 +1,28 @@
 # ShowMeEnglish Scripts and content management system
 # Features now
 # 1. search through stored subtitles(srt files), X
-# 2. able to search by each tv series, seasons
+# 2. search by each tv series, seasons
 # 3. search and present words in sentences and episodes
 # 4. print the results as txt files
+# 5. able to determine the how many lines are presented
 
 # Features to add
 # able to search by each multiple series or seasons or episodes
 # able to search phrases and words separately
-# able to determine the how many lines are presented
 # able to choose different stems, or search a lists of words 
 import glob
 import linecache
 import fnmatch
 import os
 import re
+from check_words import scope,check_lines_in_files
 #search in one or serveral specific tv series
-counter = 0 
+counter = 0  
 while counter == 0:
-	text = raw_input('Search a term or a word\n> ').lower()
+	text = raw_input('Hi!!\nLooking for a phrase or word?\n> ').lower()
 	#for windows
 	# os_path = 'C:\Users\Frank/Desktop/ScriptSearch'
+	#for linux=
 	os_path = '/home/frank/Dropbox/ScriptSearch/'
 	series = ["How I Met Your Mother", 
 			  "The Big Bang Theory",
@@ -39,67 +41,9 @@ while counter == 0:
 
 	#three different scopes, 
 	#1.pick a tv show and a season and search through them
-	if pick_series > 0 and pick_series!= 99:
-		series_name = series[int(pick_series)-1]
-		season_number = input('Which Season? Type 0 if search all seasons\n> ')	
-		if season_number > 0:
-			season = "Season %d" % season_number
-		elif season_number == 0:
-			season = "*" 
-		path = os_path + '%s/%s/*.srt' % (series_name, season)
-		files = glob.glob(path)
-	#2.pick all tv shows in the database and search through them all
-	elif pick_series == 0:
-		series_name = "*" 
-		season ="*"
-		path = os_path + '%s/%s/*.srt' % (series_name, season)
-		files = glob.glob(path)
-	#3. pick multiple tv shows and search through 
-	elif pick_series == 99:
-		numbers_user_pick = []
-		print "0.ALL\n1.How I Met Your Mother\n2.The Big Bang Theory\
-			\n3.Game of Thrones\n4.House of Cards\n5.Breaking Bad\
-			\n6.Mad Men\n7.Suits\n8.The Newsroom\n9.Sex and the City"
-		user_input = int(raw_input('Type the number of the series.\n>')) 
-		while user_input!=0:			
-			numbers_user_pick.append(user_input)
-			print "---------------------------------------------"
-			print "Here're the series you chose:\n"
-			for number in numbers_user_pick:
-				print series[number-1]
-			user_input = int(raw_input('\nPick again?Type 0 to start search\n>')) 
-		files = []
-		for number in numbers_user_pick:			
-			files = files + glob.glob(os_path +'%s/%s/*.srt' % (series[number-1],"*"))
-	#print phrases, print our the lines the prases are in
-	# lines_presented = int(raw_input('How many adjacent lines are presented with the results?'))
-	result_list = []
-	def check_phrases():
-		filename = os.path.basename(fp.name)
-		lines_presented = -2			
-		result_list.append(
-		"<Number %d match>\n<In file '%s'>\n<In line %d in the file>" % (counter,filename,i),
-			)
-		while lines_presented <= 6:
-			result_list.append(
-				linecache.getline(file, i+lines_presented)
-				)
-			lines_presented = lines_presented + 1
-		result_list.append(
-		"------------------------------------------------------\n"
-		)
-		for item in result_list:
-			print item
-		linecache.clearcache()
-	#run check phrases function with each line in each subtitles file 
-	for file in files:
-		fp = open (file, 'r')
-		for i, line in enumerate(fp):
-			if re.findall(r'%s\s|%s.|%s\,|%s\?|%s\!'%(text,text,text,text,text),line.lower()):
-				counter = counter + 1
-				check_phrases()
-		fp.close()	
-	print "Total %d matches" % counter		
+	files = scope(os_path,series,pick_series)
+	result_list = []			
+	check_lines_in_files(files,text,counter,result_list)
 	#create a txt file with the results
 	if counter > 0:
 		result_fiename = text + ".txt"
@@ -113,6 +57,3 @@ while counter == 0:
 			counter = 0
 		else:
 			counter = 0
-
-
-
